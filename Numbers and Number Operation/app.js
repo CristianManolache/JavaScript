27e43e77c2c0c264a25b28b34d2a0d6ecd33bbdc
CristianManolache <mlcristian@outlook.com>
2020-08-07 01:47:30 +0100

    Numbers and Number Operations

---
+++ Numbers and Number Operation/app.js
@@ -0,0 +1,28 @@
+//Numbers 
+
+let a = 5;
+let b = 2.25;
+
+let sum = a + b;
+console.log("The sum of a and b is: " + sum);
+let sub = a - b;
+console.log("The sub of a and b is: " + sub);
+let molt = a * b;
+console.log("The molt of a and b is: " + molt);
+let div = a / b;
+console.log("The div of a and b is: " + div);
+let perc = a % b;
+console.log("The perc(module/reminder) of a and b is: " + perc);
+console.log("////////////////////////////")
+console.log("////////////////////////////")
+
+let y = 4 + 6 + 10 * 10;
+let z = (4 + 6 + 10) * 10;
+
+console.log("The result of y 4 + 6 + 10 * 10 is: " + y);
+console.log("The result of z (4 + 6 + 10) * 10 is: " + z);
+
+
+let u = 100;
+u += 120;
+console.log("The result of u(100) adding += (120) is: " + u);
