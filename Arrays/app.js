ca35bd44b4a3b409f69dee73a37b2536fe9e9c9b
CristianManolache <mlcristian@outlook.com>
2020-08-07 07:15:50 +0100

    Basic Arrays

---
+++ Arrays/app.js
@@ -0,0 +1,15 @@
+//Arrays
+//The array is a object than can let you stroe multiple values in a single variable[collection of elements].
+//The location of an item in an array. ... Note: In most programming languages, the first array index is 0 or 1, and indexes continue through the natural numbers. The upper bound of an array is generally language and possibly system specific.
+
+let friend1 = "Cristian";
+let friend2 = "John";
+let friend3 = "Spider-Man";
+let friend4 = "Cristiano Ronaldo";
+
+let friends = new Array("Cristian","John","Spider-Man","Cristiano Ronaldo");
+
+let oldFriends = ["Cr7",66,true,null,undefined];
+
+console.log(friends[3]); //-> The output is Cristiano Ronaldo witch is in position 3 in the index of that array.
+///
