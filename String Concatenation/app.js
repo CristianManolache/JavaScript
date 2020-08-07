ed640a733fb9c105ba596f400ee47db7042c8b10
CristianManolache <mlcristian@outlook.com>
2020-08-07 01:01:45 +0100

    String Concatenation + Method

---
+++ String Concatenation/app.js
@@ -0,0 +1,22 @@
+//String Concatenation
+
+let name = 'Cristian';
+let lastName = 'Alex';
+let fullName = name + " " + lastName;
+
+
+//console.log("Hello there your name is: " + name + " " + lastName);
+console.log("Hello there your full name is: " + fullName);
+
+
+///////////////////////////////
+let info = 'google';
+let url = 'http://www.' + info + '.com';
+console.log(url);
+
+//concat() -Method
+
+const str1 = 'Hello';
+const str2 = 'World';
+
+console.log(str1.concat(" ",str2));
