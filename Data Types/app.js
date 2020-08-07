6a5842d324c2c7e27d5c013d5b1043096dc86be2
CristianManolache <mlcristian@outlook.com>
2020-08-07 06:54:15 +0100

    Primitive Data Types + Symbol(ES6)

---
+++ Data Types/app.js
@@ -0,0 +1,38 @@
+//Data Types 
+
+//String
+let text = "This is a string";
+console.log(text);
+console.log(typeof text);
+console.log("**********************");
+//Number
+let value = 1996;
+console.log(value)
+console.log(typeof value);
+console.log("**********************");
+//Boolean
+let isHuman = true;
+let isMarried = false;
+console.log(isHuman);
+console.log(typeof isHuman);
+console.log(isMarried);
+console.log("**********************");
+//Null
+let x = null;
+console.log(x);
+console.log(typeof x);
+console.log("In reality NULL is not a object but is a BUG , check on google why");
+console.log("**********************");
+//undefined
+let name;
+console.log(typeof name);
+console.log("**********************");
+//Symbol(ES6)
+/* Symbols are new primitive type introduced in ES6. Symbols are completely unique identifiers. Just like their primitive counterparts (Number, String, Boolean), they can be created using the factory function Symbol() which returns a Symbol.*/
+//Factory function
+const symbol = Symbol('description');
+console.log(symbol);
+console.log(typeof symbol);
+const directions = {  UP   : Symbol( "UP" ),  DOWN : Symbol( "DOWN" ),  LEFT : Symbol( "LEFT" ),  RIGHT: Symbol( "RIGHT" )};
+console.log(directions);
+console.log(typeof directions);
