7f4b8bdfbe81dd5920e8ffb5e791ff56c9340245
CristianManolache <mlcristian@outlook.com>
2020-08-08 02:32:51 +0100

    * "null" and "undefined" *

---
+++ Null and undefined/app.js
@@ -0,0 +1,9 @@
+//Null and undefined
+//undefined - "JavaScript can not find value for this"
+//Null - "programmer set the value"
+
+let number = 20 + null; // 20 + 0
+console.log(number);
+console.log("*****************");
+let number2 = 20 + undefined;
+console.log(number2);
