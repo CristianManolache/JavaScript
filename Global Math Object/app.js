f5f91a7a5f4fb0387f53199efb2ace7e648ebfdd
CristianManolache <mlcristian@outlook.com>
2020-08-08 15:33:31 +0100

    Global Math Object

---
+++ Global Math Object/app.js
@@ -0,0 +1,28 @@
+//Global Math Object
+
+let number = 3.4456789;
+
+let test = Math.floor(4.6655);
+console.log(test); //output 4 because of Math.floor
+/////////////////////////
+console.log("****************************");
+let number2 = 5.999;
+let result = Math.ceil(number2); //rounded number
+console.log(result);
+console.log("****************************");
+let number3 = 16;
+let result2 = Math.sqrt(number3); //square
+console.log(result2);
+console.log("****************************");
+let number4 = 4678;
+let result3 = Math.PI;
+console.log(result3);
+console.log("****************************");
+let result4 = Math.min(1,7,5,4);
+console.log(result4);
+console.log("****************************");
+let result5 = Math.max(121,32232,23141251,2341);
+console.log(result5);
+console.log("****************************");
+let result6 = Math.floor((Math.random()* 10) + 1);
+console.log(result6);
