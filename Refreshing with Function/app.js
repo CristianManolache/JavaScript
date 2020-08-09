c9bf6f504c873c55fae0431762d4deec344de956
CristianManolache <mlcristian@outlook.com>
2020-08-09 16:47:43 +0100

    Quick refresher about Functions

---
+++ Refreshing with Function/app.js
@@ -0,0 +1,34 @@
+//Create code and execute whenever you want/need
+//Adds behavior to objects (methods)
+
+function nameOfFunction(){
+    //code that will be executed
+}
+//call or invoke
+nameOfFunction();
+
+//obj.method() string.toLowerCase()
+//Function declarations function expressions
+
+//expression
+let varName =  function nameOfFunction(){
+    //code
+}
+
+varName();
+
+//parameters and arguments
+
+function nameOfFunction(firstPara,secondPara){
+    //code + parameters
+}
+
+//call or invoke
+nameOfFunction(firstPara,secondPara);
+
+//retuirn statement
+function nameOfFunction(){
+    //code
+    return//
+    //code will not run
+}
